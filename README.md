742bcf55f2c0751ec6e91f22641ce0e22cc690d6
bell2lee <paperlee@kookmin.ac.kr>
2020-06-18 02:16:49 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+- chmod +rx ./venv_set.sh 
---
+++ venv_set.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+url="./youtube"
+# virtual env
+
+sudo rm -rf "$url"
+
+git clone https://github.com/bell2lee/youtube-survey-tool.git youtube
+
+sudo python3 -m venv "$url/venv"
+source "$url/venv/bin/activate"
+
+pip install --upgrade pip
+pip install -r "$url/requirements.txt"
+
+cp ./settings.py "$url/youtube_survey_tool/settings.py"
+
+sudo python3 "$url/manage.py" makemigrations
+sudo python3 "$url/manage.py" migrate
